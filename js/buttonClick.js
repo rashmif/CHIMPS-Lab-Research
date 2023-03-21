f92520535d9eee5e160542d1dcbb3f341cc99db5
Alyssa14411 <pivrpiv@126.com>
2023-03-21 00:56:51 -0400

    add right-upper corner buttons

--- js/buttonClick.js
+++ js/buttonClick.js
@@ -1,13 +1,13 @@
 function handleClick() {
-    // // Define an array of pre-defined topics
-    // var topics = ["What is your favorite book?", "Do you prefer tea or coffee?", "What is your favorite hobby?"];
-    
-    // // Generate a random number between 0 and the length of the topics array
-    // var randomIndex = Math.floor(Math.random() * topics.length);
-    
-    // // Use DOM manipulation to display the topic on the webpage
-    document.getElementById("topic").innerHTML = "\"compared prompts\" clicked";
+    const content = document.getElementById("searchInterface");
+    if (content.classList.contains("displayed")) {
+        content.classList.remove("displayed");
+        content.classList.add("hidden");
+    } else {
+        content.classList.remove("hidden");
+        content.classList.add("displayed");
+    }
 }
 
-var myButton = document.getElementById('comparePrompts');
-myButton.onclick = handleClick;
+var button = document.getElementById('comparePrompts');
+button.onclick = handleClick;
